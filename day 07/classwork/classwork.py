f4a09f08576a2ca0da53017cb8996135ba4f02e6
andriabubu <andriaeliava09@gmail.com>
2024-08-06 02:35:31 -0700

    second commit

---
+++ day 07/classwork/classwork.py
@@ -0,0 +1,10 @@
+#storing the flight number
+flight_n = "BA0117"
+
+#storing the flight infomation
+destination = "New York"
+distance = 1580
+
+print(flight_n)
+print(destination)
+print (distance)
