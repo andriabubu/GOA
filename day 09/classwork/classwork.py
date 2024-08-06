c6c349fa13f2fcac6945f78366f74f68ea17cf2e
andriabubu <andriaeliava09@gmail.com>
2024-08-06 02:43:42 -0700

    second commit

---
+++ day 09/classwork/classwork.py
@@ -0,0 +1,5 @@
+savings1 = float(input("enter savings amount: "))
+amount_in_1_year =print(savings1 + 7.5)
+
+savings2 = float(input("enter savings amount: "))
+amount_in_1_year = print(savings2 + 12.5)
