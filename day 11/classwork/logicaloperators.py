981fd355ceef8d8fcab8c9255dfeaa3f612c365d
andriabubu <andriaeliava09@gmail.com>
2024-08-06 02:45:59 -0700

    second commit

---
+++ day 11/classwork/logicaloperators.py
@@ -0,0 +1,34 @@
+# Logical Operator:
+# 1) "and" operator
+# 2) "or" operator
+#-------------------
+# 1) პირობა
+# 2) ოპერატორი
+# 3) პირობა
+
+
+# and operator----ufro uyvars flase vinem true
+
+#     (1)  (2)  (3)
+# print(True and True) # True
+# print(True and False) # False
+# print(False and True) # False
+# print(False and False) # False
+
+
+# or operator----ufro uyvars true vinem false
+
+#    (1)  (2)  (3)
+# print(True or True) # True
+# print(True or False) # True
+# print(False or True) # True
+# print(False or False) # False
+
+
+
+
+
+
+
+
+
