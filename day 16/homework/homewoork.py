4574aa4ae9ca3553a4af743ab41a08fdf901a8e6
andriabubu <andriaeliava09@gmail.com>
2024-08-19 06:43:45 -0700

    second commit

---
+++ day 16/homework/homewoork.py
@@ -0,0 +1,23 @@
+# დავალება: გაკეთეთ for ლუპზე 10 მაგალითი.
+# 1) for i in range(30):
+#    print(i)
+# 2) for ssd in range(20):
+#    print(ssd)
+# 3) for i in range(2, 30 + 2,2):
+#    print(i)
+# 4) for i in range(1,30 + 1,2):
+#    print(i)
+# 5) for y in range(0, 10 + 1):
+#    print(str(y) + " Andria ")
+# 6) for i in range(0,10 + 1):
+#    name = input("enter your name: ")
+# 7) for y in range(0,10 + 1):
+#    break
+# 8) number = int(input("Enter your number: "))
+#for i in range(number + 1):
+#    print(i)
+# 9) number = int(input("Enter your number: "))
+#for i in range(number + 2):
+#    print(i)
+# 10) for i in range(0, 10 + 1):
+#    print(str(i) + " hello ")
